113a31212402980f7e9368a49e7e01f5170ac1a6
Jenny Huang <jhuang9@mit.edu>
2024-04-29 09:19:02 -0400

    revised spelling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
   <div class="text-container">
     <h1>Are you interested in buying in Boston?</h1>
     <p>Welcome! As one of the oldest cities in America, Boston is a charming city with over 300 years of history. The city is home to 
-    beautiful cobblestone streets, historic sites, world-clas educational institutions, as well as over 20 distinct neighborhoods, each 
+    beautiful cobblestone streets, historic sites, world-class educational institutions, as well as over 20 distinct neighborhoods, each 
     with its own characteristics and charm. In this web page, we will help you gain a better understanding of the Boston housing market,
     with a specific dive into the spculative investor activites across the city. </p>
   </div>
