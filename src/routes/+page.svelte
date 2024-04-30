b0bdd2352b31affede7878152b55654e0efddf1c
Julia Chae <julia.chae2000@gmail.com>
2024-04-30 01:02:10 -0400

    adding spacers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,25 +8,37 @@
 </script>
 
 <main>
+  <div class="spacer"></div>
   <div class="title-page">
       <h1>Understanding Boston's<br>Speculative Investment Landscape</h1>
       <p class="team-name">EliteVisTeam</p>
       <p class="team-name">Jenny, Shelley, Kimia, Julia</p>
       <hr class="separator">
   </div>
-  <div class="text-container">
-    <h1>Boston is experiencing a housing crisis simultaneously with the luxury building boom...</h1>
-    <div class="text-container">
+
+    <div class="spacer"></div> 
+    <div class="spacer"></div> 
+
+
+  <div class="text-container" id="caption">
+      <h1>Boston is experiencing a housing crisis simultaneously with the luxury building boom...</h1>
+  </div>
+      <div class="text-container">
+
       <p>which makes finding an affordable home tougher than ever for the average Bostonion.</p>
 
       <p>In the present-day Boston housing market, an annual income of nearly $200,000 is necessary for a median-priced home (Homes for Profit).
         Luxury buildings are being developed all across Boston, but many of these buildings are home to part-time residents and foreign investors - not full-time Bostonions.</p>
-      
-      <p>This data comic shows a story of a young boy Timmy and his mother, as they stroll by the Four Seasons One Dalton. 
-        Timmy notices immediately that many of the windows in the building are dark. 
-        Back at home, Timmy is upset over the fact that he has to share a bed with all of his siblings, 
-        while his mother struggles in silence between paying next month’s rent and paying her childrens' health insurance. In the final panel, 
-        the single mother decides Boston has gotten much too expensive and, alas, makes a difficult decision to move her family out.</p>
+    </div>
+    <div class="spacer"></div> 
+    <div class="spacer"></div> 
+    <div class="spacer"></div> 
+
+
+    <div class="text-container">
+          <p>This data comic follows Timmy and his mother as they pass by the Four Seasons One Dalton, noticing the dark windows. 
+          Later, Timmy expresses frustration about sharing a bed with his siblings, while his mother silently grapples with financial burdens. 
+          Ultimately, they decide to move out of Boston due to its high cost of living.</p>
     </div>
 
     <div class="container">
@@ -37,40 +49,55 @@
       <p>"A lot of these condos aren't homes, these are safety deposits in the sky" - Chuck Collins, author and program director at the Institute for Policy Studies</p>
     </div>
 
-    <h1>Are you interested in buying in Boston?</h1>
+    <div class="spacer"></div> 
+    <div class="spacer"></div>
+    <div class="spacer"></div>
 
-    <p>You've come to the right place! In this web page, we will help you gain a better understanding of the Boston housing market,
+
+    <div class="text-container">
+      <h1>Are you interested in buying in Boston?</h1>
+      <p>You've come to the right place! In this web page, we will help you gain a better understanding of the Boston housing market,
       with a specific dive into the speculative investor activities across the city.</p>
+    </div>  
 
-    <p>Are you new to Boston? Let's start by comparing Boston to some other famous cities in the U.S.</p>
-  </div>
-
-  <div class="text-container" id="caption">
-    <p>Take a Guess: Use the dropdown menu to select a major U.S. city, seeing how big of a place you can own 
+    <div class="spacer"></div> 
+    <div class="spacer"></div>
+    <div class="spacer"></div>
+    <div class="text-container" id="caption">
+    <h1>Are you new to Boston? Let's start by comparing Boston to some other famous cities in the U.S.</h1>
+      <p>Take a Guess: Use the dropdown menu to select a major U.S. city, seeing how big of a place you can own 
       if you had the equivalent of 500 sq ft. in Boston!</p>
-  </div>
-
-  <SquareFootage />
-  
-  <div class="text-container" id="caption">
+    </div>
+    <SquareFootage />
+  <div class="text-container">
     <p>As you have probably noticed from above, unfortunately Boston is not the most affordable city to live in for an 
     average American. Although new buildings are homes are being added to the city, many high-rises that are being built in are not 
     affordable for those in need of housing.</p>
   </div>
 
+    <div class="spacer"></div> 
+    <div class="spacer"></div>
+    <div class="spacer"></div>
+
   <div class="text-container" id="caption">
     <p>Curious about the jaw-dropping costs of some of the upscale luxury buildings? Take a wild guess below to discover how many MIT graduate students 
     could afford a year's rent with just the price of one luxurious unit.</p>
   </div>
-
   <LuxuryApartment />
 
-  <div class="text-container" id="caption">
+  <div class="spacer"></div> 
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+
+
+  <div class="text-container">
     <p>Scroll to find out some surprising general trends about investor activity in Boston zipcodes as a whole.</p>
   </div>
   <HomeBuyers />
-
-  <PredictiveTool />
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+  <div class="spacer"></div>
 
   <div class="text-container">
     <p>As a next step, you may be curious about investment activity and price trends for the zipcode you are interested in buying in. In order to allow users to see the exact zip code they would like to buy in, we allow for the map to highlight zip codes of interest and focus on the recent speculative investment trends.</p>
@@ -78,6 +105,21 @@
   <div class="container">
     <img src = "map.png" alt="" style="width: 1200px;"/>
   </div>
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+
+
+  <div class="text-container">
+    <p>If you want to compare specific zip codes, enter them in the comparison tool below!</p>
+  </div>
+  <PredictiveTool />
+
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+  <div class="spacer"></div>
+  <div class="spacer"></div>
 
   <div class="text-container">
     <h1>Resources for New Homebuyers</h1>
